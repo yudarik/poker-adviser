e46c7afb24474936814977c4388036eff6a8698c
Arik Yudin <yudarik@gmail.com>
2017-08-31 10:22:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,9 @@ while (true) {
     let hand = _.take(cards, 5);
     let topFive = _.takeRight(cards, 5);
 
-    console.log(`Your hand cards are: ${hand}`);
-    console.log(`Top 5 cards of deck are: ${topFive}`);
-
     let bestHandValue = poker.calcBestHand(cards);
 
     console.log(`Hand: ${print(hand)} Deck: ${print(topFive)} Best hand: ${bestHandValue}`);
-
-   /* _.forEach(bestHandValue, (value) => {
-        console.log(value);
-    })*/
 }
 
 function print(array) {
